a49cc3284a66076873cef0a78b91b00198884c1e
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2026-05-12 19:45:49 +0000

    continue restore gcafsv1 for dev support

--- scripts/stats/global_chem/exevs_stats_global_chem_atmos_grid2obs.sh
+++ scripts/stats/global_chem/exevs_stats_global_chem_atmos_grid2obs.sh
@@ -29,7 +29,7 @@ export finalstat=${DATA}/final  # config variable
 mkdir -p ${finalstat}
 
 export CMODEL=`echo ${MODELNAME} | tr a-z A-Z`  # define config variable
-vmodel=`echo ${gefs_ver} | awk -F"." '{print $1}'`
+vmodel=`echo ${gcafs_ver} | awk -F"." '{print $1}'`
 export VMODEL=${CMODEL}
 
 export CONFIGevs=${CONFIGevs:-${PARMevs}/metplus_config/${STEP}/${COMPONENT}/${RUN}_${VERIF_CASE}}
@@ -123,7 +123,7 @@ for ObsType in ${grid2obs_list}; do
           aday=`echo ${adate} |cut -c1-8`
           acyc=`echo ${adate} |cut -c9-10`
           if [ "${acyc}" == "${mdl_cyc}" ]; then
-            fcst_file=${EVSINprep}/${RUN}.${aday}/${MODELNAME}/${acyc}/${RUN}/pgrb2ap25/${MODELNAME}.${RUN}.t${acyc}z.a2d_0p25.f${filehr}.reduced.grib2
+            fcst_file=${EVSINprep}/${RUN}.${aday}/${MODELNAME}/${acyc}/products/${RUN}/grib2/0p25/${MODELNAME}.${RUN}.t${acyc}z.0p25.f${filehr}.trim.grib2
             if [ -s ${fcst_file} ]; then
               echo "${fhr} found"
               echo ${fhr} >> ${recorded_temp_list}
